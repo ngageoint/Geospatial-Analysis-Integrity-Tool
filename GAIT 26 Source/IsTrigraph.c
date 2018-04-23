e34f57bd12567ff18e78fd903497211c416dfe49
Roy Rathbun <roy.rathbun@gmail.com>
2018-04-23 13:37:42 -0400

    Update IsTrigraph.c
    Updated citation

--- GAIT 26 Source/IsTrigraph.c
+++ GAIT 26 Source/IsTrigraph.c
@@ -1,19 +1,15 @@
 /************
-GAIT (Geospatial Analysis Integrity Tool) is a geospatial data validation tool developed by the Institute for Defense Analyses (IDA) for the National Geospatial-Intelligence Agency (NGA).  
+The GAIT Tool was developed at the National Geospatial-Intelligence Agency (NGA) in collaboration with the   
+Institute for Defense Analyses (IDA) under a Government contract. The government is releasing this software   
+to increase the impact of government investments by providing developers with the opportunity to take things   
+in new directions. The software use, modification, and distribution rights are stipulated within the MIT license.
 
-This source code was used to generate GAIT 26 executable software in accordance with Amendment 6 to Task Order DH-8-3815 under Contract HQ0034-14-D-0001.
-
-IDA is furnishing this item "as is". IDA was not tasked or funded to generate developer documentation or to provide support for this source code. IDA does not provide any warranty of the item whatsoever, whether express, implied, or statutory, including, but not limited to, any warranty of fitness for a particular purpose or any warranty that the contents of the item will be error-free. In no event shall NGA or IDA be held liable for damages arising, directly or indirectly, from the use of this source code. 
-
-This material may be reproduced by the U.S. Government pursuant to its unlimited use rights under DFARS 252.227-7014 [Feb 2014].
-
-The Institute for Defense Analyses (IDA) is a Federally Funded Research and Development Center that provides scientific and technical expertise on issues important to national security to the Office of the Secretary of Defense, Joint Staff, Unified Commands, and Defense Agencies. 
-
-© 2017 Institute for Defense Analyses
-  4850 Mark Center Drive
-  Alexandria, Virginia 22311-1882
-  703.845-2000
-  www.ida.org
+As-is warranty is covered within the MIT license but it's re-stated here for clarity: The GAIT tool contains several   
+software components. The software is provided "as is," and no warranty, express or implied, including but not limited   
+to the implied warranties of merchantability and fitness for particular purpose or arising by statute or otherwise in   
+law or from a course of dealing or usage in trade, is made with respect to the accuracy or functioning of the software.   
+Neither NGA or IDA are liable for any claims, losses, or damages arising from or connected with the use of the software.   
+The user's sole and exclusive remedy is to stop using the software.
 ************/
 int NumCountryCodes = 277;
 
