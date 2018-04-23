24e63c71ab31c616940358bb423e63b9f6495fb8
Roy Rathbun <roy.rathbun@gmail.com>
2018-04-23 13:46:56 -0400

    Update SEEIT_API.h
    Updated citation

--- GAIT 26 Source/SEEIT_API.h
+++ GAIT 26 Source/SEEIT_API.h
@@ -11,69 +11,6 @@ law or from a course of dealing or usage in trade, is made with respect to the a
 Neither NGA or IDA are liable for any claims, losses, or damages arising from or connected with the use of the software.   
 The user's sole and exclusive remedy is to stop using the software.
 ************/
-/****
-  This software is provided openly and freely for use in representing and
-  interchanging environmental data & databases.
-  
-  This software was developed for use by the United States Government with
-  unlimited rights.  The software was developed under IDA Contract Number
-  DASW01-98-C-0067 by the Simulation Center, Institute for Defense Analyses,
-  1801 N. Beauregard Street, Alexandria, VA, 22311-1772.  The software is
-  unclassified and is deemed as Distribution A, approved for Public Release.
-  
-  Use by others is permitted only upon the ACCEPTANCE OF THE TERMS AND
-  CONDITIONS, AS STIPULATED UNDER THE FOLLOWING PROVISIONS:
-  
-  1. Recipient may make unlimited copies of this software and give
-  copies to other persons or entities as long as the copies contain
-  this NOTICE, and as long as the same copyright notices that
-  appear on, or in, this software remain.
-  
-  2. Trademarks. All trademarks belong to their respective trademark
-  holders.  Third-Party applications/software/information are
-  copyrighted by their respective owners.
-  
-  3. Recipient agrees to forfeit all intellectual property and
-  ownership rights for any version created from the modification
-  or adaptation of this software, including versions created from
-  the translation and/or reverse engineering of the software design.
-  
-  4. Transfer.  Recipient may not sell, rent, lease, or sublicense
-  this software or any derivative.  Recipient may, however,
-  enable another person or entity the rights to use this software,
-  provided that this AGREEMENT and NOTICE is furnished along with
-  the software and/or software system utilizing this software.
-  
-  All derivatives, revisions, modifications, created by the
-  Recipient, to this software and/or related technical data shall
-  be forwarded by the Recipient to the Government at the following
-  address:
-  
-  Engineering Directorate
-  Attention SEDRIS COR
-  STRICOM
-  12350 Research Parkway
-  Orlando, FL 32826
-  
-  or via electronic mail to:  se-mgmt@sedris.org
-  
-  5. No Warranty. This software is being delivered to you AS IS
-  and there is no warranty, EXPRESS or IMPLIED, as to its use
-  or performance.
-  
-  The RECIPIENT ASSUMES ALL RISKS, KNOWN AND UNKNOWN, OF USING
-  THIS SOFTWARE.  The DEVELOPER EXPRESSLY DISCLAIMS, and the
-  RECIPIENT WAIVES, ANY and ALL PERFORMANCE OR RESULTS YOU MAY
-  OBTAIN BY USING THIS SOFTWARE OR DOCUMENTATION.  THERE IS
-  NO WARRANTY, EXPRESS OR, IMPLIED, AS TO NON-INFRINGEMENT OF
-  THIRD PARTY RIGHTS, MERCHANTABILITY, OR FITNESS FOR ANY
-  PARTICULAR PURPOSE.  IN NO EVENT WILL THE DEVELOPER, THE
-  UNITED STATES GOVERNMENT OR ANYONE ELSE ASSOCIATED WITH THE
-  DEVELOPMENT OF THIS SOFTWARE BE HELD LIABLE FOR ANY CONSEQUENTIAL,
-  INCIDENTAL OR SPECIAL DAMAGES, INCLUDING ANY LOST PROFITS
-  OR LOST SAVINGS WHATSOEVER.
-  ******/
-
 
 /* Total number of condition types defined at this release 
 ***THIS IS ALSO CO_DEFINED IN SEEIT_API.h*****/
